70ca968b0567d26e0d18c84255991f598533d311
José Martín Rodriguez Mortaloni <76118394+JoseMRodriguezM@users.noreply.github.com>
2022-07-07 12:21:07 -0300

    Commit of some changes in the configurations files

--- init.lua
+++ init.lua
@@ -1,17 +1,5 @@
-require "options"
-require "impatient"
-
-local async
-async = vim.loop.new_async(
-        vim.schedule_wrap(
-            function ()
-                require "plugins.init"
-                require "plugins.configs.bufferline"
-
-                require "theme.init"
-                
-                async:close()
-            end
-        )
-)
-async:send()
+require("packerInit")
+require("autocommands")
+require("settings")
+require("keymaps")
+require("colors")
--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,44 +1,89 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+local status_ok = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
 	return
 end
 
-configs.setup({
-	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-    -- use_languagetree = true,
-		enable = true, -- false will disable the whole extension
-		-- disable = { "css", "html" }, -- list of language that will be disabled
-		disable = { "css" }, -- list of language that will be disabled
-		additional_vim_regex_highlighting = true,
+require("nvim-treesitter.configs").setup({
+	sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
+	ensure_installed = {
+		"python",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"c_sharp",
+		"query",
+		"markdown",
+		"norg",
+		"json",
+		"json5",
+		"java",
+		"bash",
+    "comment",
+    "regex",
 	},
+	ignore_install = { "" }, -- List of parsers to ignore installing
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "yaml", "python", "css" } },
+	autotag = {
+		enable = true,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = false,
+	},
+	matchup = {
+		enable = true,
+	},
+	refactor = {
+		highlight_definitions = {
+			enable = false,
+			clear_on_cursor_move = true,
+		},
+		highlight_current_scope = {
+			enable = false,
+		},
+		smart_rename = {
+			enable = true,
+			keymaps = {
+				smart_rename = "gr",
+			},
+		},
+		navigation = {
+			enable = true,
+			keymaps = {
+				goto_definition = "<leader>td",
+				list_definitions = "<leader>tl",
+			},
+		},
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_files_lines = nil,
+		colors = { "#d9bc8c", "#a988b0", "#8da3b9" },
+	},
+	indent = { enable = true, disable = { "yaml" } },
 	context_commentstring = {
 		enable = true,
 		enable_autocmd = false,
 	},
-	autotag = {
-		enable = true,
-		disable = { "xml" },
-	},
-	rainbow = {
-		enable = true,
-		colors = {
-			"Gold",
-			"Orchid",
-			"DodgerBlue",
-			-- "Cornsilk",
-			-- "Salmon",
-			-- "LawnGreen",
+	textobjects = {
+		select = {
+			enable = true,
+
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
+
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
 		},
-		disable = { "html" },
-	},
-	playground = {
-		enable = true,
 	},
 })
